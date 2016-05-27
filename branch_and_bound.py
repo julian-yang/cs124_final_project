dca4ae62cee259ab05ff2374a16844d2ae1cb112
Julian Yang <yang.julian@gmail.com>
2016-05-26 17:53:12 -0700

    Add stat recording for total_branches, branches_explored, avgCase1 and avgCase2 prunes.

--- branch_and_bound.py
+++ branch_and_bound.py
@@ -2,6 +2,7 @@ from greedy import generate_permutations
 from genotype_generator import generate_genotype_input
 from greedy import greedy_with_regret_solver
 from greedy import convert_input
+from itertools import combinations
 from solutions_checker import check_solution2
 from time import localtime, strftime, time
 import datetime
@@ -32,7 +33,8 @@ def determine_h1_h2_h3_h4(genotype, genotype_solutions, haplotype_resolution):
     (h1, h2) = genotype_solutions[genotype][0]
     (h3, h4) = genotype_solutions[genotype][1]
     if len(haplotype_resolution[h1]) + len(haplotype_resolution[h2]) != 3 \
-            or len(haplotype_resolution[h3]) + len(haplotype_resolution[h4]) != 3:
+            or len(haplotype_resolution[h3]) + \
+                    len(haplotype_resolution[h4]) != 3:
         return h1, h2, h3, h4, False
     if haplotype_resolution[h1] == 2:
         temp = h1
@@ -66,8 +68,11 @@ def branch_and_bound(input_genotypes, greedy_solution_count, greedy_solution):
     haplotype_resolution = {}
 
     genotypes_with_two_resolutions = []
+    total_branches = 1
+    # generate genotype_resolutions and calculate total # of branches
     for genotype in unique_genotypes:
         genotype_resolutions[genotype] = get_solution_pairs(genotype)
+        total_branches *= len(genotype_resolutions[genotype])
         for (h1, h2) in genotype_resolutions[genotype]:
             if h1 in haplotype_resolution:
                 haplotype_resolution[h1].add(genotype)
@@ -80,15 +85,18 @@ def branch_and_bound(input_genotypes, greedy_solution_count, greedy_solution):
 
     # now we need to prune solutions
     # Prune Case 1: Remove all extra resolutions with only coverage 2
+    case_1_prunes = 0
     for genotype, solutions in genotype_resolutions.iteritems():
         # solutions is a list containing tuples of haplotype resolutions
         found_coverage2 = False
         for (h1, h2) in solutions:
-            if len(haplotype_resolution[h1]) + len(haplotype_resolution[h2]) == 2:
+            if len(haplotype_resolution[h1]) + \
+                    len(haplotype_resolution[h2]) == 2:
                 if not found_coverage2:
                     found_coverage2 = True
                 else:
                     solutions.remove((h1, h2))
+                    case_1_prunes += 1
         if len(solutions) == 2:
             genotypes_with_two_resolutions.append(genotype)
 
@@ -96,6 +104,7 @@ def branch_and_bound(input_genotypes, greedy_solution_count, greedy_solution):
     # Mi has (h1, h2) and (h4, h5) and Mj has (h2, h3) and (h5, h6)
     # if h1, h3, h4, h6 have coverage 1 and h2 and h4 have coverage 2:
     # keep only (h1, h2) and (h2, h3) [delete (h4, h5) and (h5, h6)
+    case_2_prunes = 0
     for genotype1 in genotypes_with_two_resolutions:
         if len(genotype_resolutions[genotype1]) != 2:
             continue
@@ -118,12 +127,14 @@ def branch_and_bound(input_genotypes, greedy_solution_count, greedy_solution):
                 # genotype)
                 genotype_resolutions[genotype1].pop()
                 genotype_resolutions[genotype2].pop()
+                case_2_prunes += 2
 
     for solutions in genotype_resolutions.itervalues():
         if not solutions:
             print 'empty solutions list!'
 
-    unique_genotypes.sort(key=lambda x: len(genotype_resolutions[x]))
+    unique_genotypes.sort(key=lambda x: 2 if len(genotype_resolutions[x]) > 1
+                                        else 1)
     # perform depth first search of solutions space
     # cur_solution is a map of genotype -> resolution tuple
     cur_solution = {}
@@ -134,10 +145,12 @@ def branch_and_bound(input_genotypes, greedy_solution_count, greedy_solution):
     opt_solution = {}
     opt_haploptype_count = greedy_solution_count - 1
     found_opt_solution = False
+    branches_explored = 0
     while stack:
         (cur_genotype, cur_genotype_resolutions) = stack[-1]
         # pick first resolution off stack.
         cur_resolution = cur_genotype_resolutions[-1]
+        branches_explored += 1
         # append haplotypes / update haplotype count
         for haplotype in cur_resolution:
             if not haplotype in solution_haplotypes:
@@ -186,14 +199,16 @@ def branch_and_bound(input_genotypes, greedy_solution_count, greedy_solution):
                                           unique_haplotype_count)
 
     if not found_opt_solution:
-        return greedy_solution_count, greedy_solution
+        return greedy_solution_count, greedy_solution, total_branches, \
+               branches_explored, case_1_prunes, case_2_prunes
 
     final_solution = []
     for genotype in input_genotypes:
         pair = opt_solution[genotype]
         final_solution.append(pair[0])
         final_solution.append(pair[1])
-    return opt_haploptype_count, final_solution
+    return opt_haploptype_count, final_solution, total_branches, \
+           branches_explored, case_1_prunes, case_2_prunes
 
 
 def write_out(output_string, output_file):
@@ -207,6 +222,10 @@ def run_test(N, M, L, runs, csv_file):
     avg_size = 0
     total_opt = 0
     total_greedy = 0
+    avg_total_branches = 0
+    avg_branches_explored = 0
+    avg_case_1_prunes = 0
+    avg_case_2_prunes = 0
     folder = 'generated_data'
     output_file_name = 'generated_data/N_{}_M_{}_L_{}.txt'.format(N, M, L)
     outputfile = open(output_file_name, 'w')
@@ -216,6 +235,7 @@ def run_test(N, M, L, runs, csv_file):
         input_genotypes = \
             generate_genotype_input(N=N, M=M, L=L, print_output=False)
         converted_genotypes = convert_input(input_genotypes)
+        # run greedy
         greedy_start = time()
         (greedy_haplotypes, solution) = \
             greedy_with_regret_solver(converted_genotypes)
@@ -228,10 +248,21 @@ def run_test(N, M, L, runs, csv_file):
                                                         solution),
                                     greedy_time),
                   outputfile)
+        # run branch_and_bound
         start = time()
-        (opt_haplotypes_count, opt_solution) = \
-            branch_and_bound(converted_genotypes, len(greedy_haplotypes), solution)
+        (opt_haplotypes_count, opt_solution, total_branches, branches_explored,
+         case_1_prunes, case_2_prunes) = \
+            branch_and_bound(converted_genotypes,
+                             len(greedy_haplotypes), solution)
         end = time()
+        write_out('total_branches: {}'.format(total_branches), outputfile)
+        write_out('branches_explored: {}'.format(branches_explored), outputfile)
+        write_out('case1_prunes: {}'.format(case_1_prunes), outputfile)
+        write_out('case2_prunes: {}'.format(case_2_prunes), outputfile)
+        avg_total_branches += total_branches
+        avg_branches_explored += branches_explored
+        avg_case_1_prunes += case_1_prunes
+        avg_case_2_prunes += case_2_prunes
         opt_elapsed = end - start
         opt_time = str(datetime.timedelta(seconds=opt_elapsed))
         write_out('{} {} {}'.format(opt_haplotypes_count,
@@ -239,11 +270,14 @@ def run_test(N, M, L, runs, csv_file):
                                                     opt_solution),
                                     opt_time),
                   outputfile)
-
         total_opt += opt_elapsed
         total_greedy += greedy_elapsed
         avg_diff += len(greedy_haplotypes) - opt_haplotypes_count
         avg_size += len(greedy_haplotypes)
+    avg_total_branches /= float(runs)
+    avg_branches_explored /= float(runs)
+    avg_case_1_prunes /= float(runs)
+    avg_case_2_prunes /= float(runs)
     avg_diff /= float(runs)
     avg_size /= float(runs)
     avg_benefit = avg_diff / avg_size
@@ -254,16 +288,23 @@ def run_test(N, M, L, runs, csv_file):
     avg_time_diff = avg_greedy_time - avg_opt_time
     avg_time_diff_str = ''
     if avg_greedy_time > avg_opt_time:
-        avg_time_diff_str = str(datetime.timedelta(seconds=avg_greedy_time-avg_opt_time))
+        avg_time_diff_str = '-' + str(datetime.timedelta(
+            seconds=avg_greedy_time-avg_opt_time))
     else:
-        avg_time_diff_str = '-' + str(datetime.timedelta(seconds=avg_opt_time-avg_greedy_time))
+        avg_time_diff_str = str(datetime.timedelta(
+            seconds=avg_opt_time-avg_greedy_time))
 
     write_out('avg diff: {}'.format(avg_diff), outputfile)
     write_out('avg size: {}'.format(avg_size), outputfile)
     write_out('avg benefit: {}'.format(avg_benefit), outputfile)
-    write_out('avg greedy run-time: {}'.format(avg_greedy_time_str), outputfile)
+    write_out('avg greedy run-time: {}'.format(avg_greedy_time_str),
+              outputfile)
     write_out('avg opt run-time: {}'.format(avg_opt_time_str), outputfile)
     write_out('avg time diff: {}'.format(avg_time_diff_str), outputfile)
+    write_out('avg total branches: {}'.format(avg_total_branches), outputfile)
+    write_out('avg branches explored: {}'.format(avg_branches_explored), outputfile)
+    write_out('avg case 1 prunes: {}'.format(avg_case_1_prunes), outputfile)
+    write_out('avg case 2 prunes: {}'.format(avg_case_2_prunes), outputfile)
     csv_file.write('{}, '.format(N))
     csv_file.write('{}, '.format(M))
     csv_file.write('{}, '.format(L))
@@ -278,6 +319,10 @@ def run_test(N, M, L, runs, csv_file):
     csv_file.write('{}, '.format(avg_greedy_time_str))
     csv_file.write('{}, '.format(avg_opt_time_str))
     csv_file.write('{}, '.format(avg_time_diff_str))
+    csv_file.write('{}, '.format(avg_total_branches))
+    csv_file.write('{}, '.format(avg_branches_explored))
+    csv_file.write('{}, '.format(avg_case_1_prunes))
+    csv_file.write('{}, '.format(avg_case_2_prunes))
     csv_file.write('\n')
 
 
@@ -298,11 +343,11 @@ def main():
     #print solution
     #(opt_haplotypes_count, opt_solution) = \
     #   branch_and_bound(test1, 7)
-    runs = 20
+    runs = 10
     N = 30
     M = 10
-    L_start = 95
-    L_end = 101
+    L_start = 40
+    L_end = 70
     L_step = 5
     now_time = strftime("%Y-%m-%d_%H-%M-%S", localtime())
     csv_file_name = 'generated_data/test_' + now_time + '.csv'
@@ -310,6 +355,7 @@ def main():
     csv_file.write('N, M, L, avg_diff, avg_size, avg_benefit, ' +
                    'avg_greedy_time_secs, avg_opt_time_secs, avg_time_diff_secs, ' +
                    'avg_greedy_time, avg_opt_time, avg_time_diff' +
+                   'avg_total_branches, avg_branches_explored, avg_case_1_prunes, avg_case_2_prunes'
                    '\n')
     for i in xrange(L_start, L_end, L_step):
         run_test(N, M, i, runs, csv_file)
